0ea335cd334e087db86fb8d43cb71ff5a9fd356d
KevinCantrell <cantrell@up.edu>
2020-03-12 12:42:24 -0700

    calculated residuals from polynomial fit

--- co2.py
+++ co2.py
@@ -3,6 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+def oscFunc(x, amp, period, offset):  
+    return amp/2 * np.cos((x + offset)  * 2 * np.pi/period) 
+
+def plyFunc(x,c0,c1,c2):
+    return c0+x*c1+x**2*c2
+
 dfCarbonDioxide=pd.read_table('co2_mlo.txt',delimiter=r"\s+",skiprows=146)
 dfCarbonDioxide['date']=pd.to_datetime(dfCarbonDioxide[['year', 'month', 'day', 'hour', 'minute', 'second']])
 boolMissing=dfCarbonDioxide['value']==-999.99
@@ -14,3 +20,14 @@ print(dfCarbonDioxide)
 fig, ax = plt.subplots(figsize=(12,8))
 ax.plot(dfCarbonDioxide['date'],dfCarbonDioxide['value'],'ok')
 ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+
+startDate=min(dfCarbonDioxide['date'])
+timeElapsed=dfCarbonDioxide['date']-startDate
+daysSinceStart=timeElapsed.dt.days
+
+coeffs=np.polyfit(daysSinceStart,dfCarbonDioxide['value'],2)
+
+predicted=plyFunc(daysSinceStart,coeffs[2],coeffs[1],coeffs[0])
+
+fig,axResidual=plt.subplot()
+axResidual.plot(dfCarbonDioxide['date'],dfCarbonDioxide['value']-predicted,'ok')
