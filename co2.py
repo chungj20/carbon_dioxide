f70b04c64187a857d60e3d2519940df2247e1597
chris-elkhal <elkhal20@up.edu>
2020-03-12 12:42:25 -0700

    Calculated residuals from polyfit

--- co2.py
+++ co2.py
@@ -2,6 +2,13 @@ import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from scipy.optimize import curve_fit
+
+def oscFunc(x, amp, period, offset):  
+    return amp/2 * np.cos((x + offset)  * 2 * np.pi/period) 
+
+def plyFunc(x, c0, c1, c2):
+    return c0+c1*x+c2*x**2
 
 dfCarbonDioxide=pd.read_table('co2_mlo.txt',delimiter=r"\s+",skiprows=146)
 dfCarbonDioxide['date']=pd.to_datetime(dfCarbonDioxide[['year', 'month', 'day', 'hour', 'minute', 'second']])
@@ -14,3 +21,23 @@ print(dfCarbonDioxide)
 fig, ax = plt.subplots(figsize=(12,8))
 ax.plot(dfCarbonDioxide['date'],dfCarbonDioxide['value'],'ok')
 ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+
+startDate=min(dfCarbonDioxide['date'])
+timeElapsed=dfCarbonDioxide['date']-startDate
+daysSinceStart=timeElapsed.dt.days
+#x variable is daysSinceStart
+
+coeffs = np.polyfit(daysSinceStart, dfCarbonDioxide['value'], 2)
+#popt, pcov = curve_fit(lineFit, daysSinceStart, dfCarbonDioxide['value']))
+predicted = plyFunc(daysSinceStart, coeffs[2], coeffs[1], coeffs[0])
+
+fig, axResidual = plt.subplots()
+axResidual.plot(dfCarbonDioxide['date'],dfCarbonDioxide['value']-predicted,'ok')
+
+
+
+periodHolder = 365.25
+
+
+
+
