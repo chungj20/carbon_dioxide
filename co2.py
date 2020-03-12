fc39af8c1f73bed2e638a28679594009d822afb5
KevinCantrell <cantrell@up.edu>
2020-03-12 10:28:46 -0700

    Initial commit

---
+++ co2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+dfCarbonDioxide=pd.read_table('co2_mlo.txt',delimiter=r"\s+",skiprows=146)
+dfCarbonDioxide['date']=pd.to_datetime(dfCarbonDioxide[['year', 'month', 'day', 'hour', 'minute', 'second']])
+boolMissing=dfCarbonDioxide['value']==-999.99
+dfCarbonDioxide[boolMissing]=np.nan
+dfCarbonDioxide=dfCarbonDioxide.dropna()
+dfCarbonDioxide=dfCarbonDioxide.reset_index(drop=True)
+print(dfCarbonDioxide)
+
+fig, ax = plt.subplots(figsize=(12,8))
+ax.plot(dfCarbonDioxide['date'],dfCarbonDioxide['value'],'ok')
+ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
